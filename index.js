7df289f8e4746e6da0c8b38ae682488466f96da8
m3yevn <mmm.myintmyat@gmail.com>
2020-06-08 01:18:50 +0800

    Gitlestial Commit-Gen 0

--- index.js
+++ index.js
@@ -4,33 +4,52 @@ const { generateCommit } = require("./src/commit-gen");
 const { initGit } = require("./src/init");
 const { addGit } = require("./src/add");
 const { commitGit } = require("./src/commit");
+const { testCommand } = require("./helpers/command-helper");
 
-function main() {
-  const subcommand = process.argv[2];
-  if (!subcommand) {
-    console.log("Gitlestial - Version: " + package.version);
-    console.log(
-      "Use gitlestial --help for viewing all the available commands."
-    );
-    return;
-  }
+async function main() {
+  try {
+    const subcommand = process.argv[2];
+    if (!subcommand) {
+      console.log("Gitlestial - Version: " + package.version);
+      console.log(
+        "Use gitlestial --help for viewing all the available commands."
+      );
+      return;
+    }
 
-  switch (subcommand) {
-    case "init":
-      initGit();
-      break;
-    case "add":
-      addGit();
-      break;
-    case "commit":
-      commitGit();
-      break;
-    case "commit-gen":
-      generateCommit();
-      break;
-    default:
-      displayHelp(subcommand);
-      break;
+    const gitTest = await testCommand("git");
+    if (!gitTest) {
+      throw new Error(
+        "Git CLI is not installed. Please install git cli from https://git-scm.com/downloads"
+      );
+    }
+
+    const bfgTest = await testCommand("bfg");
+    if (!bfgTest) {
+      throw new Error(
+        "Bfg CLI is not installed. Please install bfg cli from https://rtyley.github.io/bfg-repo-cleaner/"
+      );
+    }
+
+    switch (subcommand) {
+      case "init":
+        initGit();
+        break;
+      case "add":
+        addGit();
+        break;
+      case "commit":
+        commitGit();
+        break;
+      case "commit-gen":
+        generateCommit();
+        break;
+      default:
+        displayHelp(subcommand);
+        break;
+    }
+  } catch (ex) {
+    console.log(ex.message);
   }
 }
 
