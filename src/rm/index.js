d24410ca5d40a9f9e8dff9c7aba7f2a098acaa13
m3yevn <mmm.myintmyat@gmail.com>
2020-06-08 02:11:13 +0800

    Gitlestial Commit-Gen

--- src/rm/index.js
+++ src/rm/index.js
@@ -4,9 +4,6 @@ const { logError } = require("../../helpers/error-helper");
 function removeGit(filePath) {
   return new Promise((resolve, reject) => {
     const child = spawn("git", ["rm", filePath]);
-    child.stdout.on("data", (chunk) => {
-      console.log(chunk.toString("utf8"));
-    });
     child.on("error", (error) => {
       logError(error);
       reject(error);
