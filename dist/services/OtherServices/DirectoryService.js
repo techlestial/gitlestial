5c68e234e31cdef335cd715fe068284619ad22b7
m3yevn <mmm.myintmyat@gmail.com>
2020-06-13 15:30:17 +0800

    Gitlestial Commit-gen

--- dist/services/OtherServices/DirectoryService.js
+++ dist/services/OtherServices/DirectoryService.js
@@ -1,24 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.removeDirectory = void 0;
+var rimraf_1 = __importDefault(require("rimraf"));
 var fs_1 = require("fs");
 exports.removeDirectory = function (path) {
-    if (fs_1.existsSync(path)) {
-        var files = fs_1.readdirSync(path);
-        if (files.length > 0) {
-            files.forEach(function (filename) {
-                if (fs_1.statSync(path + "/" + filename).isDirectory()) {
-                    exports.removeDirectory(path + "/" + filename);
-                }
-                else {
-                    fs_1.unlinkSync(path + "/" + filename);
-                }
-            });
-            fs_1.rmdirSync(path);
-        }
-        else {
-            fs_1.rmdirSync(path);
-        }
-    }
+    return new Promise(function (resolve, reject) {
+        fs_1.exists(path, function (isExists) {
+            if (isExists) {
+                rimraf_1.default(path, function (hasError) {
+                    if (hasError) {
+                        return reject();
+                    }
+                    return resolve();
+                });
+            }
+            return reject();
+        });
+    });
 };
 //# sourceMappingURL=DirectoryService.js.map
