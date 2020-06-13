ae2520435c5f588145272812dfb9038d76eabd7b
m3yevn <mmm.myintmyat@gmail.com>
2020-06-13 15:34:27 +0800

    Gitlestial Commit-gen

--- dist/services/OtherServices/DirectoryService.js
+++ dist/services/OtherServices/DirectoryService.js
@@ -8,17 +8,15 @@ var rimraf_1 = __importDefault(require("rimraf"));
 var fs_1 = require("fs");
 exports.removeDirectory = function (path) {
     return new Promise(function (resolve, reject) {
-        fs_1.exists(path, function (isExists) {
-            if (isExists) {
-                rimraf_1.default(path, function (hasError) {
-                    if (hasError) {
-                        return reject(hasError);
-                    }
-                    return resolve(true);
-                });
-            }
-            return reject(path + " does not exist");
-        });
+        if (fs_1.existsSync(path)) {
+            rimraf_1.default(path, function (hasError) {
+                if (hasError) {
+                    return reject(hasError);
+                }
+                return resolve(true);
+            });
+        }
+        return reject(path + " does not exist");
     });
 };
 //# sourceMappingURL=DirectoryService.js.map
