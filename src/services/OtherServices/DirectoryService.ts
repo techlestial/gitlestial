019626ef5e3502df6eeed4978896a8ef5bc421bc
m3yevn <mmm.myintmyat@gmail.com>
2020-06-13 15:34:27 +0800

    Gitlestial Commit-gen

--- src/services/OtherServices/DirectoryService.ts
+++ src/services/OtherServices/DirectoryService.ts
@@ -1,19 +1,17 @@
 import { logInfo } from "./LogService";
 import rimraf from "rimraf";
-import { exists } from "fs";
+import { exists, existsSync } from "fs";
 
 export const removeDirectory = (path: string) => {
   return new Promise((resolve, reject) => {
-    exists(path, (isExists) => {
-      if (isExists) {
-        rimraf(path, (hasError) => {
-          if (hasError) {
-            return reject(hasError);
-          }
-          return resolve(true);
-        });
-      }
-      return reject(path+" does not exist");
-    });
+    if (existsSync(path)) {
+      rimraf(path, (hasError) => {
+        if (hasError) {
+          return reject(hasError);
+        }
+        return resolve(true);
+      });
+    }
+    return reject(path + " does not exist");
   });
 };
