41ebe727abbf916fdef05da5ada2bd9bca258bf4
m3yevn <pyaehlianmoe@gmail.com>
2020-06-30 18:10:12 +0800

    update: test files

--- src/services/OtherServices/LoadService.ts
+++ src/services/OtherServices/LoadService.ts
@@ -8,8 +8,6 @@ export class LoadService {
   constructor() {}
 
   load = (blockAmount: number) => {
-    std.write("\x1b[32m");
-    std.write("\x1b[40m");
     const blockLine: string[] = [];
 
     for (var i = 0; i < blockAmount * 2; i++) {
@@ -17,7 +15,9 @@ export class LoadService {
     }
     blockLine.push(arrow);
 
-    std.write(blockLine.join("") + `    ${blockAmount * 5}% Completed`);
+    std.write(
+      "\x1b[32m" + blockLine.join("") + `    ${blockAmount * 5}% Completed`
+    );
     rdl.cursorTo(std, 0);
   };
 }
