08b37bbf79804e2872e33bda1bacaf07190089a2
m3yevn <omegasoon@gmail.com>
2020-06-30 18:00:48 +0800

    Gitlestial Commit-gen

--- src/services/GitServices/GitCommitGenService.ts
+++ src/services/GitServices/GitCommitGenService.ts
@@ -122,16 +122,11 @@ const getRandomNumber = (maxNum: number) => {
 
 const cleanUpGitCommitFile = async (amount: number) => {
   try {
-    logInfo("Complete committing for " + amount + " times");
-    await spawnProcess("bfg", [
-      "--delete-files",
-      fileName,
-      "--no-blob-protection",
-    ]);
     await spawnProcess("git", ["rm", "-f", filePath]);
   } catch (ex) {
     logError(ex);
   } finally {
+    logInfo("Complete committing for " + amount + " times");
     logInfo("Now do git push to your repository and voila!");
   }
 };
