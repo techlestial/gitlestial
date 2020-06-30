20d6ec2f5d743dcba9a3c5825ab572671b028067
m3yevn <omegasoon@gmail.com>
2020-06-30 18:06:52 +0800

    update: test files

--- dist/services/OtherServices/SpinService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.spin = void 0;
-exports.spin = function (current) {
-};
-//# sourceMappingURL=SpinService.js.map
