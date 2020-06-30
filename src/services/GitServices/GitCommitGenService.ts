7d01f9f0ae6a7dac8763b59bd0d16883019acbaa
m3yevn <omegasoon@gmail.com>
2020-06-30 17:57:02 +0800

    Gitlestial Commit-gen

--- src/services/GitServices/GitCommitGenService.ts
+++ src/services/GitServices/GitCommitGenService.ts
@@ -2,34 +2,42 @@ import { spawnProcess } from "../OtherServices/SpawnService";
 import { logInfo, logError } from "../OtherServices/LogService";
 import { CheckIfArgIncludes } from "../OtherServices/CommandService";
 import { writeFileSync, existsSync } from "fs";
+import { LoadService } from "../OtherServices/LoadService";
 
 const folderName = ".gitlestial";
 const fileName = ".commit";
 const filePath = process.cwd() + `/${folderName}/` + fileName;
+const loader = new LoadService();
 
 export const generateCommit = async () => {
   let amount: number = 1,
     contributors: string[] = [];
   try {
-    if(!existsSync(folderName)){
+    if (!existsSync(folderName)) {
       await spawnProcess("mkdir", [folderName]);
     }
-    if(!existsSync(filePath)){
+
+    if (!existsSync(filePath)) {
       await spawnProcess("touch", [filePath]);
     }
+
     await spawnProcess("git", ["add", filePath]);
     amount = getAmount();
     logInfo("Committing for " + amount + " times");
     logInfo("Do not terminate this process!");
+
     const hasContributors = CheckIfArgIncludes("--contributors");
     const commitMessage = CheckIfArgIncludes("-m");
     if (hasContributors) {
       contributors = getContributors(hasContributors);
     }
+
     for (var i = 0; i < amount; i++) {
+      amountPercentageLoader(i, amount);
       if (hasContributors && contributors.length) {
         await setConfigUserEmail(contributors);
       }
+
       writeFileSync(filePath, i.toString());
       await spawnProcess("git", [
         "commit",
@@ -42,6 +50,69 @@ export const generateCommit = async () => {
     logError(ex);
   } finally {
     cleanUpGitCommitFile(amount);
+  }
+};
+
+const amountPercentageLoader = (current: number, limit: number) => {
+  if (current === limit * 0.05) {
+    loader.load(1);
+  }
+  if (current === limit * 0.1) {
+    loader.load(2);
+  }
+  if (current === limit * 0.15) {
+    loader.load(3);
+  }
+  if (current === limit * 0.2) {
+    loader.load(4);
+  }
+  if (current === limit * 0.25) {
+    loader.load(5);
+  }
+  if (current === limit * 0.3) {
+    loader.load(6);
+  }
+  if (current === limit * 0.35) {
+    loader.load(7);
+  }
+  if (current === limit * 0.4) {
+    loader.load(8);
+  }
+  if (current === limit * 0.45) {
+    loader.load(9);
+  }
+  if (current === limit * 0.5) {
+    loader.load(10);
+  }
+  if (current === limit * 0.55) {
+    loader.load(11);
+  }
+  if (current === limit * 0.6) {
+    loader.load(12);
+  }
+  if (current === limit * 0.65) {
+    loader.load(13);
+  }
+  if (current === limit * 0.7) {
+    loader.load(14);
+  }
+  if (current === limit * 0.75) {
+    loader.load(15);
+  }
+  if (current === limit * 0.8) {
+    loader.load(16);
+  }
+  if (current === limit * 0.85) {
+    loader.load(17);
+  }
+  if (current === limit * 0.9) {
+    loader.load(18);
+  }
+  if (current === limit * 0.95) {
+    loader.load(19);
+  }
+  if (current === limit) {
+    loader.load(20);
   }
 };
 
