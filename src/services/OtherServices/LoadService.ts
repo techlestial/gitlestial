74ce2471934ccafb41a3a9c36e867235e5d48988
m3yevn <mmm.myintmyat@gmail.com>
2020-06-30 18:09:03 +0800

    update: test files

--- src/services/OtherServices/LoadService.ts
+++ src/services/OtherServices/LoadService.ts
@@ -8,14 +8,16 @@ export class LoadService {
   constructor() {}
 
   load = (blockAmount: number) => {
+    std.write("\x1b[32m");
+    std.write("\x1b[40m");
     const blockLine: string[] = [];
 
-    for (var i = 0; i < blockAmount*2; i++) {
+    for (var i = 0; i < blockAmount * 2; i++) {
       blockLine.push(block);
     }
     blockLine.push(arrow);
 
-    std.write(blockLine.join("") + `    ${blockAmount*5}% Completed`);
+    std.write(blockLine.join("") + `    ${blockAmount * 5}% Completed`);
     rdl.cursorTo(std, 0);
   };
 }
