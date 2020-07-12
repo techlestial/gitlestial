4ada23ee1a8a1bdc5bb41bb72115026601eabef7
pyaehlianmoe <techlestial@gmail.com>
2020-07-12 18:25:46 +0100

    udpated dist folder

---
+++ dist/config/gitlestial.config.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = {
+    commitGen: {
+        amount: 10,
+        contributors: ["techlestial@gmail.com"],
+        message: "Test triggering commits",
+    },
+};
+//# sourceMappingURL=gitlestial.config.js.map
